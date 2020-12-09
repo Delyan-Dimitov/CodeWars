d9da3a6b1b61d27bad024297437d8f9a8a3aad1e
Delyan Dimitrov <delyandimitrov99@gmail.com>
2020-12-09 20:58:42 +0200

    Rot13

---
+++ rot13/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace rot13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Rot13("Grfg"));
+        }
+        public static string Rot13(string message)
+        {
+            int incrementor = 13;
+            var sb = new StringBuilder();
+            for (int i = 0; i < message.Length; i++)
+            {
+                char current = message[i];
+                if (Regex.IsMatch(current.ToString(), "^[A-Z]*$"))
+                {
+                    if ((int)current >= 110)
+                    {
+                        sb.Append((char)(current - (char)incrementor));
+                    }
+                    sb.Append((char)(current + (char)incrementor));
+                }
+               
+                else if (Regex.IsMatch(current.ToString(), "^[A-Z]*$"))
+                {
+                    if ((int)current >= 78)
+                    {
+                        sb.Append((char)(current - (char)incrementor));
+                    }
+                    sb.Append((char)(current + (char)incrementor));
+                }
+                else
+                {
+                    sb.Append(current);
+                }
+
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
